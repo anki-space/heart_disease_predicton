78c6e1333ea8b878260d515975d0c2c30908ad72
anki-space <ankitkumar18122003@gmail.com>
2024-10-24 15:19:35 +0530

    Heart_disease_prediction

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=15, required=True)
+    dob = forms.DateField(required=True, widget=forms.DateInput(attrs={'type': 'date'}))
+    hospital_name = forms.CharField(required=True, max_length=100)
+ 
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'dob', 'hospital_name', 'password1', 'password2']
+
+
+        
